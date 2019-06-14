b38390762b7ee1ed86fdf19ea05a4cce8ebdddda
r0man <roman@burningswell.com>
2019-06-14 21:23:31 +0200

    Add deploy repositories

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/r0man/ring-graphql-multipart"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :author "r0man"
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]]
   :plugins [[jonase/eastwood "0.3.5"]]
