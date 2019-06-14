bdba8bc762c43bc2b3246e42d61045d7158cdf02
r0man <roman@burningswell.com>
2019-06-14 21:24:59 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ring-graphql-multipart "0.1.0-SNAPSHOT"
+(defproject ring-graphql-multipart "0.1.0"
   :description "Ring middleware for GraphQL multipart form requests"
   :url "https://github.com/r0man/ring-graphql-multipart"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
